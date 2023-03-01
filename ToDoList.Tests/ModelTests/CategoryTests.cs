9c97d112820727716463125f29404ed295bfe1e6
John Lenz <john.lenz27@gmail.com>
2023-03-01 11:13:32 -0800

    Added new test file CategoryTests

---
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System;
+
+namespace ToDoList.Tests
+{
+    [TestClass]
+    public class CategoryTests
+    {
+
+        [TestMethod]
+        public void CategoryConstructor_CreatesInstanceOfCategory_Category()
+        {
+            Category newCategory = new Category("test category");
+            Assert.AreEqual(typeof(Category), newCategory.GetType());
+        }
+
+        [TestMethod]
+        public void GetName_ReturnsName_String()
+        {
+
+            string name = "Test Category";
+            Category newCategory = new Category(name);
+            string result = newCategory.Name;
+            Assert.AreEqual(name, result);
+        }
+
+    }
+}
