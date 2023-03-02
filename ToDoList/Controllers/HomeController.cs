1f81be34b22103ed9817af6c77d96c144bf68863
John Lenz <john.lenz27@gmail.com>
2023-03-02 09:20:20 -0800

    updated file structure to allow cs and img folders

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -5,11 +5,17 @@ namespace ToDoList.Controllers
     public class HomeController : Controller
     {
 
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/favorite_photos")]
+        public ActionResult FavoritePhotos()
+        {
+            return View();
+        }
 
     }
 }
